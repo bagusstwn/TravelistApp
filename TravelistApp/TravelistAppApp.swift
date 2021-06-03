9631eab227a82a89af7cccbc8968851ec25942f7
Bagus setiawan <setiawanbagus4315@gmail.com>
2021-06-03 08:39:11 +0700

    add files

---
+++ TravelistApp/TravelistAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelistAppApp.swift
+//  TravelistApp
+//
+//  Created by Bagus setiawan on 03/06/21.
+//
+
+import SwiftUI
+
+@main
+struct TravelistAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
