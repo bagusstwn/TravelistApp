76fc5dba7db87a4d02e66fc7839262d148a25f85
Bagus setiawan <setiawanbagus4315@gmail.com>
2021-06-04 13:54:35 +0700

    add CustomRoundedCorners view !

--- TravelistApp/ContentView.swift
+++ TravelistApp/ContentView.swift
@@ -15,35 +15,44 @@ struct ContentView: View {
         NavigationView{
             List{
                 ForEach(feth.datatotal, id: \.id){ a in
-                    HStack{
-                        Image(systemName: "rectangle")
-                        VStack(alignment: .leading){
-                            Text("\(a.name)")
-                                .font(.title)
-                                .fontWeight(.bold)
-                            Text("\(a.address)")
-                                .font(.callout)
-                                .foregroundColor(.secondary)
-                        }
-                        
-                        Spacer()
+                    ZStack{
+                        RectangleBG()
                         HStack{
-                            Image(systemName: "heart")
-                                .foregroundColor(.red)
-                            Text("\(a.like)")
+                            Image(systemName: "")
+                                .frame(width: 93, height: 92, alignment: .center)
+                                .background(CustomRoundedCorners(color: Color.blue, tl: 20, tr: 0, bl: 20, br: 0))
+                            
+                            VStack(alignment: .leading){
+                                Text("\(a.name)")
+                                    .font(.title2)
+                                    .fontWeight(.bold)
+                                    .lineLimit(1)
+                                Text("\(a.address)")
+                                    .font(.callout)
+                                    .foregroundColor(.secondary)
+                                    .lineLimit(1)
+                            }
+                            
+                            Spacer()
+                            HStack{
+                                Image(systemName: "heart")
+                                    .foregroundColor(.red)
+                                Text("\(a.like)")
+                            }
+                            .padding()
                         }
-                        
                     }
                 }
             }
+            
             .listStyle(PlainListStyle())
             .navigationTitle("Travelist")
             .navigationBarItems(trailing: Button(action: {}){
                 Image(systemName: "person.circle.fill")
+                    .frame(width: 50.0, height: 50.0)
+                    .font(.largeTitle)
             })
         }
-        
-       
     }
 }
 
