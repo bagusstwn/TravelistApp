4cb1aa1eb56cc1deb719dd615a58d187eef8e03e
Bagus setiawan <setiawanbagus4315@gmail.com>
2021-06-04 13:54:08 +0700

    add RectangleBG view !

---
+++ TravelistApp/RectangleBG.swift
@@ -0,0 +1,25 @@
+//
+//  RectangleBG.swift
+//  TravelistApp
+//
+//  Created by Bagus setiawan on 04/06/21.
+//
+
+import SwiftUI
+
+struct RectangleBG: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 20)
+            .fill(Color.white)
+            .frame(width: 380, height: 93, alignment: .center)
+            .shadow(color: Color.black.opacity(0.2), radius: 3, x: 2, y: 5)
+            
+            
+    }
+}
+
+struct RectangleBG_Previews: PreviewProvider {
+    static var previews: some View {
+        RectangleBG()
+    }
+}
