7f26d8d73a83425f6c2b1b34b084b9490b06e22d
Bagus setiawan <setiawanbagus4315@gmail.com>
2021-06-04 21:07:06 +0700

    add profileView !

---
+++ TravelistApp/ProfileView.swift
@@ -0,0 +1,34 @@
+//
+//  ProfileView.swift
+//  TravelistApp
+//
+//  Created by Bagus setiawan on 04/06/21.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        VStack{
+            MapKit()
+                .frame(height: 300)
+            CircleProfileImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading){
+                Text("Bagus Setiawan")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Text("Setiawanbagus4315@gmail.com")
+            }
+            Divider()
+            Spacer()
+        }
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
